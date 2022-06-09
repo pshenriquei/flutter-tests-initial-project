27e1d699e5331831997ac24dc9b7984f9f9cd5a6
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-09 11:12:49 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,35 @@
+import 'package:bytebank/database/dao/contact_dao.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
 void main() {
-  runApp(BytebankApp());
+  runApp(BytebankApp(
+    contactDao: ContactDao(),
+  ));
 }
 
 class BytebankApp extends StatelessWidget {
+  final ContactDao contactDao;
+
+  BytebankApp({@required this.contactDao});
 
   @override
   Widget build(BuildContext context) {
-
     final ThemeData theme = ThemeData();
 
     return MaterialApp(
       theme: theme.copyWith(
-        colorScheme:theme.colorScheme.copyWith( primary: Colors.green[900], secondary: Colors.blueAccent[700]),
+        colorScheme: theme.colorScheme.copyWith(
+            primary: Colors.green[900], secondary: Colors.blueAccent[700]),
         buttonTheme: ButtonThemeData(
           buttonColor: Colors.blueAccent[700],
           textTheme: ButtonTextTheme.primary,
         ),
       ),
-      home: Dashboard(),
+      home: Dashboard(
+        contactDao: contactDao,
+      ),
     );
   }
 }
