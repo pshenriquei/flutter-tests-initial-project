3033522c3c354731f0a4e7ba39089088b7d93ac3
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-09 11:12:53 -0300

    Update contact_form.dart

--- lib/screens/contact_form.dart
+++ lib/screens/contact_form.dart
@@ -3,15 +3,21 @@ import 'package:bytebank/models/contact.dart';
 import 'package:flutter/material.dart';
 
 class ContactForm extends StatefulWidget {
+
+  final ContactDao contactDao;
+  ContactForm({@required this.contactDao});
+
   @override
-  _ContactFormState createState() => _ContactFormState();
+  _ContactFormState createState() => _ContactFormState(contactDao: contactDao);
 }
 
 class _ContactFormState extends State<ContactForm> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _accountNumberController =
       TextEditingController();
-  final ContactDao _dao = ContactDao();
+      
+  final ContactDao contactDao;
+  _ContactFormState({@required this.contactDao});
 
   @override
   Widget build(BuildContext context) {
@@ -49,14 +55,14 @@ class _ContactFormState extends State<ContactForm> {
               padding: const EdgeInsets.only(top: 16.0),
               child: SizedBox(
                 width: double.maxFinite,
-                child: RaisedButton(
+                child: ElevatedButton(
                   child: Text('Create'),
-                  onPressed: () {
+                  onPressed: () async {
                     final String name = _nameController.text;
-                    final int accountNumber =
-                        int.tryParse(_accountNumberController.text);
+                    final int accountNumber = int.tryParse(_accountNumberController.text);
                     final Contact newContact = Contact(0, name, accountNumber);
-                    _dao.save(newContact).then((id) => Navigator.pop(context));
+                    await contactDao.save(newContact);
+                    Navigator.pop(context);
                   },
                 ),
               ),
