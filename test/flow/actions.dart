70678e91f10c40476965910858c3263696ad6499
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-09 23:01:22 -0300

    Update actions.dart

--- test/flow/actions.dart
+++ test/flow/actions.dart
@@ -9,3 +9,27 @@ Future<void> clickOnTheFeatureItem(WidgetTester tester) async {
   expect(transferFeatureItem, findsWidgets);
   return tester.tap(transferFeatureItem);
 }
+
+Future<void> clickOntheElevatedButtonWithText(
+    WidgetTester tester, String text) async {
+  final createButton = find.widgetWithText(ElevatedButton, text);
+  expect(createButton, findsOneWidget);
+  return tester.tap(createButton);
+}
+
+Future<void> fillTextFieldWithTextLabel(
+  WidgetTester tester, {
+  String text = '',
+  @required String labelText,
+}) async {
+  final nameTextField = find.byWidgetPredicate(
+      (widget) => textFieldByLabelTextMatcher(widget, labelText));
+  expect(nameTextField, findsOneWidget);
+  return tester.enterText(nameTextField, text);
+}
+
+Future<void> clickOnTheFabNew(WidgetTester tester) async {
+  final fabNewContact = find.widgetWithIcon(FloatingActionButton, Icons.add);
+  expect(fabNewContact, findsOneWidget);
+  return tester.tap(fabNewContact);
+}
