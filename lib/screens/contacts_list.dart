2e0f36d2d351afc885d5237c397b86dc2b3e58da
Pedro Henrique <82547663+pshenriquei@users.noreply.github.com>
2022-06-09 23:01:11 -0300

    Update contacts_list.dart

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,26 +1,22 @@
 import 'package:bytebank/components/progress.dart';
-import 'package:bytebank/database/dao/contact_dao.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:bytebank/screens/transaction_form.dart';
+import 'package:bytebank/widgets/app.dependencies.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatefulWidget {
 
-  final ContactDao contactDao;
-  ContactsList({@required this.contactDao});
-
   @override
-  _ContactsListState createState() => _ContactsListState(contactDao: contactDao);
+  _ContactsListState createState() => _ContactsListState();
 }
 
 class _ContactsListState extends State<ContactsList> {
 
-  final ContactDao contactDao;
-  _ContactsListState({@required this.contactDao});
 
   @override
   Widget build(BuildContext context) {
+    final dependencies = AppDependencies.of(context);
     
     return Scaffold(
       appBar: AppBar(
@@ -28,7 +24,7 @@ class _ContactsListState extends State<ContactsList> {
       ),
       body: FutureBuilder<List<Contact>>(
         initialData: [],
-        future: contactDao.findAll(),
+        future: dependencies.contactDao.findAll(),
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.none:
@@ -66,7 +62,7 @@ class _ContactsListState extends State<ContactsList> {
           Navigator.of(context)
               .push(
                 MaterialPageRoute(
-                  builder: (context) => ContactForm(contactDao: contactDao),
+                  builder: (context) => ContactForm(),
                 ),
               )
               .then((value) => setState(() {}));
